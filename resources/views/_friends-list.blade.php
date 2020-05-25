1731bbc54bfc312da61373a220779d0f941f4621
Milad Awad <millad.99.awad@gmail.com>
2020-05-25 22:47:24 +0300

    designed the timeline

---
+++ resources/views/_friends-list.blade.php
@@ -0,0 +1,18 @@
+<h3 class="font-bold text-xl mb-4">Friends</h3>
+<ul>
+    @foreach(range(1, 8) as $index)
+        <li class="mb-4">
+            <div class="flex items-center text-sm">
+                <img
+                    src="https://avatars.dicebear.com/api/avataaars/m.svg?mood[]=happy"
+                    alt=""
+                    height="40"
+                    width="40"
+                    class="rounded-full mr-2"
+                >
+
+                John Doe
+            </div>
+        </li>
+    @endforeach
+</ul>
---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,21 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img
+            src="https://avatars.dicebear.com/api/avataaars/m.svg?mood[]=happy"
+            alt=""
+            height="50"
+            width="50"
+            class="rounded-full mr-2"
+        >
+    </div>
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+        <p>
+            Hi, I'm Jeffrey. I'm the creator of Laracasts and spend most of my days building the site
+            and thinking of new ways to teach confusing concepts. I live in Orlando, Florida with my
+            wife and two kids. Before we can dive into writing the core logic, let's first set aside
+            fifteen minutes or so
+            to design the main timeline page, using Tailwind.
+        </p>
+    </div>
+</div>
